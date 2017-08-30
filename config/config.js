58caefdc3b829bfa1cd954b3758e70f527d4f52a
julcsar13 <julcsar13@gmail.com>
2017-08-30 01:16:04 -0500

    confing

--- config/config.js
+++ config/config.js
@@ -1,5 +1,7 @@
 
 module.exports = {
 	port :process.env.PORT || 8000,
-	db:process.env.MONGODB || "mongodb://localhost:27017/ciistacna"
+	db:process.env.MONGOLAB_URI || process.env.MONGOHQ_URL || "mongodb://localhost:27017/ciistacna"
+
+
 }
