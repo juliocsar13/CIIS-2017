65e41e2eed19ddc8ea542213262554c6c75c2a9f
julcsar13 <julcsar13@gmail.com>
2017-08-29 22:19:37 -0500

    add route postmaster

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,11 @@
 var express = require('express');
 var router = express.Router();
+
 var userController = require('../controllers/userController');
+var postMasterController = require('../controllers/postMasterController');
+
 var mailController = require('../controllers/mail');
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
@@ -16,4 +20,7 @@ router.route('/usuarios')
         .get(userController.getUsers);
 router.route('/Dusuarios')
         .get(userController.deleteUsers);
+
+router.route('/postmaster')
+		.get(postMasterController.createView)
 module.exports = router;
